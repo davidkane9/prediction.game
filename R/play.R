98cb26ec50fe9b86e2ee96b1b2c09c34b7616cdf
David Kane <dave.kane@gmail.com>
2019-07-21 15:38:03 -0700

    Lots more comments as a guide to Jeff or anyone else who can help me!

--- R/play.R
+++ R/play.R
@@ -20,6 +20,55 @@
 #' @examples
 #' play(data = 1:10, n = 5, 3, 7, sample, size = 1)
 
+# Big picture: I want to pass in functions like "median(sample(data$x, 5,
+# replace = FALSE))". Can't figure out how! Happy to have weird syntax for
+# accessing the data which is passed in. Or happy to hack this by just assuming
+# that the data exists in the environment and is not actually passed in.
+
+# Here are some notes on our current problem. First, consider some quotes from
+# Advanced R:
+
+# "Each time a function is called, a new environment is created to host
+# execution. This is called the execution environment, and its parent is the
+# function environment."
+
+# "An execution environment is usually ephemeral; once the function has
+# completed, the environment will be garbage collected."
+
+# "This happens because every time a function is called a new environment is
+# created to host its execution. This means that a function has no way to tell
+# what happened the last time it was run; each invocation is completely
+# independent."
+
+# The issue seems to be that, when map_dbl() and other functions are executed
+# within another function, we don't get the natural change in the random seed
+# which we would expect. This works great:
+
+# map_dbl(.x = 1:5, .f = ~ runif(1))
+# [1] 0.4758260 0.1840778 0.4199492 0.5779564 0.8983577
+
+# This does not!
+
+# my_func <- function(FUN){map_dbl(.x = 1:5, .f = ~ FUN)}
+# my_func(runif(1))
+# [1] 0.7555603 0.7555603 0.7555603 0.7555603 0.7555603
+
+# Argg! That is no good. It is as if the environment which is created for each
+# call to runif() within the map_dbl() gets deleted before R "knows" to evolve
+# the random seed.
+
+# Note that even using rerun --- as we do in the code below --- does not work by
+# itself.
+
+# my_func <- function(FUN){unlist(rerun(.n = 5,  FUN))}
+# > my_func(runif(1))
+# [1] 0.7477465 0.7477465 0.7477465 0.7477465 0.7477465
+
+# So it seems like FUN(data, ...) --- as in the code below --- is key. My guess
+# is that the presence of the data argument forces R to "notify" itself that it
+# is making a call --- to sample() in the case of the current code --- which then
+# causes the random seed to evolve, which leads to 5 different answers.
+
 play <- function(data, n, guess_1, guess_2, FUN, ...){
   
   # Need more argument error checking and at least a few test cases.
@@ -35,15 +84,17 @@ play <- function(data, n, guess_1, guess_2, FUN, ...){
   # But, right now, we are just passing in the name of the tibble. We don't
   # mention the variable names. The function (FUN) argument is just something
   # like sample. But we don't know which column in "data" the function "sample"
-  # should be applied to. For now, we just hard code it. Must be a better way!
+  # should be applied to. For now, only allow you to pass in a vector. Must be a
+  # better way!
   
   # Second, how to handle complex functions, especially ones that take
   # arguments. Right now, we use ... which works fine in these simple cases. But
   # I am not sure that this scales to more complex situations. In particular, I
-  # would like FUN to be able to be a pipe . . .
+  # would like FUN to be able to be a chain of pipes . . .
   
   # For now, as long as data is a vector and FUN works on vectors, everything
-  # should work as advertized. Then, of course, data should be renamed x.
+  # should work as advertized. Then, for Tidyverse standards, data (since it is
+  # a vector) should be renamed x.
 
   # Next version, data should be (or at least allowed to be) a tibble and then
   # this won't work (unless we check for the two cases explicitly). And that is
@@ -60,8 +111,12 @@ play <- function(data, n, guess_1, guess_2, FUN, ...){
   # "jumping" out of the functions environment?) and, so, you get the same
   # sample each time. So, I think we have to pass the data in somehow.
   
+  # Or, actually, this issue of no-randomness seems to be caused by something
+  # other than the passing in of data with the function call.
+  
   # Something like map_dbl(1:{{n}}, ~ FUN) seens to work OK, but we need a way
-  # for FUN to refer to stuff in the local environment only.
+  # for FUN to refer to stuff in the local environment only, except for the
+  # no-randomness.
   
   # Maybe this should be a Shiny app? Would then need to pre-load all the data
   # sets we care about.
@@ -88,7 +143,7 @@ play <- function(data, n, guess_1, guess_2, FUN, ...){
     # the next line. But I can't quite figure out why . . .
     
     mutate(answer = unlist(rerun(.n = {{n}}, FUN(data, ...)))) %>% 
-  
+    
     mutate(winner = case_when(
        abs(guess_1 - answer) <  abs(guess_2 - answer) ~ "guess_1",
        abs(guess_1 - answer) >  abs(guess_2 - answer) ~ "guess_2",
