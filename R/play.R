d9b19a81877de21c23e7ff2088f3fc9e511c7c6e
David Kane <dave.kane@gmail.com>
2019-07-22 14:38:43 -0700

    save all these comments from play()

--- R/play.R
+++ R/play.R
@@ -25,48 +25,12 @@
 #' table(z$winner)
 
 # I am not even sure I understand this second example! I would have guessed that
-# the best guess is 20.09, which is the result of mean(mtcars$mpg) would win.
-# But not quite! A lower number is better because there is a bit of a right skew
-# to mpg, resulting in the median being lower than the mean. But the simple
-# median is not the best guess either. Best is a number somewhere in between the
-# mean and the median. Tricky! Presumably, if size were 1, then the median would
-# be best and if size were 32 (entire data set), then the mean would be best . .
-# .
-
-# Need a vignette. Need a function, show(), which takes, via %>%, the result of
-# play() and creates a cool animation using d3rain.
-
-# Should we connect all this to bootstrapping? What we have, implicitly (?), is
-# a competition over sort-of bootstraped samples . . .
-
-# Should we connect all this to the concept to cross-validation? That is, the
-# same (sort of?) framework as what we have here --- lots of experiments, each
-# using a different sample of the data --- is like cross-validation. Instead of
-# taking a sample, you divude the data into 10 sets training and test data, run
-# the two competing processes each on the training, and then compare how they do
-# on the test.
-
-# Issue 1: Need to be able to run contests in which the formula does not produce
-# a single number. Or do we? Hmmm. 
-
-# Issue 2: No longer hard-code the evaluation function. Right now, we use the
-# absolute value of the distance to evaluate the winner. But what if we wanted
-# the squared distance? Or is this a bit of a trick question since the winner in
-# both metrics is the same . . .
-
-# Big question: What if, instead of declaring a winner each draw, we want to sum
-# up all the errors (absolute or squared or cubed or whatever) and then use that
-# sum to determine the winner? This would suggest that, within an individual
-# contest, we might compare our two guesses against a draw of 10 or 100 or
-# whatever answers from the formula.
-
-# Of course, we might still want to do that n times . . .
-
-# Bigger (?) question: Do we need for guesses to be formulas? Not today.
-# Calculate your guess however you want, using whatever statistical tricks you
-# want. But, at that point, your guess is just as number, as is mine, and we are
-# going to see who wins. Any randomness comes from the formula. 
-
+# the best guess is 20.09, which is the result of mean(mtcars$mpg). But not
+# quite! A lower number is better because there is a bit of a right skew to mpg,
+# resulting in the median being lower than the mean. But the simple median is
+# not the best guess either. Best is a number somewhere in between the mean and
+# the median. Tricky! Presumably, if size were 1, then the median would be best
+# and if size were 32 (entire data set), then the mean would be best . . .
 
 
 play <- function(n, guess_1, guess_2, formula){
@@ -77,7 +41,7 @@ play <- function(n, guess_1, guess_2, formula){
   
   # Maybe this should be a Shiny app? Would then need to pre-load all the data
   # sets we care about.
-  
+  # 
   # We should not need to calculate an empty tibble at the start. Instead, we
   # should be using map_df() (or something like it) which will produce a tibble
   # automatically.
@@ -108,7 +72,7 @@ play <- function(n, guess_1, guess_2, formula){
   # chaining, which will display the result in some pleasing fashion. Check out
   # the d3rain package. Maybe the argument will be called show()? A dual column
   # histogram that fills over time would be cool!
-  
+  # 
   # Do we really need entire columns with guess_1 and guess_2 repeated n times?
   # Probably not!
   
